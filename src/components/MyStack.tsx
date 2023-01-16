3fd8c2319a25c5df15a8780dcf9d092c7c701795
Javier Garcia <trad.ljgarcia@gmail.com>
2023-01-16 16:30:12 -0300

    fixed images bug

--- src/components/MyStack.tsx
+++ src/components/MyStack.tsx
@@ -4,17 +4,17 @@ export function MyStack () {
     return (
         <>
         <Container id="stack">
-            <img className="icons" src="../public/images/005-html-file.png" />
-            <img className="icons" src="../public/images/002-cascading-style-sheets.png" />
-            <img className="icons" src="../public/images/003-js.png" />
-            <img className="icons" src="../public/images/004-physics.png" />
-            <img className="icons" src="../public/images/redux.png" />
-            <img className="icons" src="../public/images/007-nodejs.png" />
-            <img className="icons" src="../public/images/express.png" />
-            <img className="icons" src="../public/images/mongodb.png" />
-            <img className="icons" src="../public/images/sql-server.png" />
-            <img className="icons" src="../public/images/008-ruby.png" />
-            <img className="icons" src="../public/images/006-bootstrap.png" />
+            <img className="icons" src="/005-html-file.png" />
+            <img className="icons" src="/002-cascading-style-sheets.png" />
+            <img className="icons" src="/003-js.png" />
+            <img className="icons" src="/004-physics.png" />
+            <img className="icons" src="/redux.png" />
+            <img className="icons" src="/007-nodejs.png" />
+            <img className="icons" src="/express.png" />
+            <img className="icons" src="/mongodb.png" />
+            <img className="icons" src="/sql-server.png" />
+            <img className="icons" src="/008-ruby.png" />
+            <img className="icons" src="/006-bootstrap.png" />
         </Container>
         </>
     )

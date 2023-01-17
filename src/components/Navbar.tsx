a70c38898599ff5f8a1c177461272162094294fa
Javier Garcia <trad.ljgarcia@gmail.com>
2023-01-16 21:42:24 -0300

    fixed linkedin link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export function Navbar() {
             <Nav className="ms-auto">
                 <Nav.Link id="navlinks" to="/" as={NavLink}>Home</Nav.Link>
                 <Nav.Link id="navlinks" to="/contact" as={NavLink}>Contact Me</Nav.Link>
-                <Nav.Link id="navlinks" href="www.linkedin.com/in/javier-garcía-3653a795">LinkedIn</Nav.Link>
+                <Nav.Link id="navlinks" href="https://www.linkedin.com/in/javier-garc%C3%ADa-3653a795/">LinkedIn</Nav.Link>
                 <Nav.Link id="navlinks" href="https://github.com/xavi-garcia">GitHub</Nav.Link>
             </Nav>
         </Container>
